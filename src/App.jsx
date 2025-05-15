69f707ab76d7da1ae97fd457a2daacb3d7834c0a
Sakshi Jain <sakshijainst@gmail.com>
2025-05-15 11:49:44 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Signup from "./Signup";
 
 function App() {
   return (
     <>
       <h1 class="text-3xl font-bold underline ">Hello world!</h1>
+      <Signup/>
     </>
   );
 }
